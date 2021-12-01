5275391b9832a29372c12aa1fe37a6f63b06f459
lakshay123-ctrl <70754884+lakshay123-ctrl@users.noreply.github.com>
2021-12-01 18:52:57 +0530

    Add files via upload

---
+++ 121project.py
@@ -0,0 +1,23 @@
+import cv2
+import time
+import numpy as np
+
+video = cv2.VideoCapture(0)
+img = cv2.imread("121image.jpg")
+while True:
+    ret,frame = video.read()
+    frame = cv2.resize(frame,(640,480))
+    img = cv2.resize(img,(640,480))
+    u_black = np.array([104,153,70])
+    l_black = np.array([30,30,0])
+    mask = cv2.inRange(frame,l_black,u_black)
+    res = cv2.bitwise_and(frame,frame,mask = mask)
+    f = frame-res
+    f  = np.where(f==0,image,f)
+    cv2.imshow("video","frame")
+    cv2.imshow("mask",f)
+    if cv2.waitKey(1):
+        break
+
+video.release()
+cv2.destroyAllWindows()
